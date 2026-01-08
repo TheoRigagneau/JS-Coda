04428b30056b712e799e4dd5231044dd097dcd09
TheoRigagneau <trigagneau@gmail.com>
2026-01-08 11:33:46 +0100

    part2

---
+++ script.js
@@ -0,0 +1,24 @@
+const name = "B1-A";
+console.log(name);
+let student = 28;
+console.log(student);
+let classe = true;
+console.log(classe);
+//part 2 
+let eleve1 = {
+    prenom : "Théo",
+    note_maths : 15,
+    note_français : 12
+};
+console.log(eleve1.prenom);
+//part 3 
+let eleve2 = {
+    prenom : "Lilian",
+    note_maths : 4,
+    note_français : 9.5
+};
+let eleve3 = {
+    prenom : "Pierre",
+    note_maths : 14,
+    note_français : 0.5
+};
