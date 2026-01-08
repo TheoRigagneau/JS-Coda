dda5ac1cc5b20aab7fbb799f46e7e799f1894beb
TheoRigagneau <trigagneau@gmail.com>
2026-01-08 11:49:41 +0100

    part4

--- script.js
+++ script.js
@@ -4,17 +4,23 @@ let student = 28;
 console.log(student);
 let classe = true;
 console.log(classe);
+
+console.log("==============");
 //part 2 
+console.log("Part 2");
 let eleve1 = {
     prenom : "Théo",
     note_maths : 15,
     note_français : 12
 };
 console.log(eleve1.prenom);
+
+console.log("==============");
 //part 3 
+console.log("Part 3");
 let eleve2 = {
     prenom : "Lilian",
-    note_maths : 4,
+    note_maths : 14,
     note_français : 9.5
 };
 let eleve3 = {
@@ -22,3 +28,23 @@ let eleve3 = {
     note_maths : 14,
     note_français : 0.5
 };
+
+let eleve = [eleve1, eleve2, eleve3];
+for (let i = 0; i < eleve.length; i++) {
+    console.log(eleve[i].prenom);
+}
+
+console.log("==============");
+// part 4
+console.log("Part 4");
+for (let i = 0; i<eleve.length; i++) {
+    let moyenne=(eleve[i].note_français+eleve[i].note_maths)/2;
+    console.log(eleve[i].prenom, moyenne);
+    if (moyenne <10) {
+        console.log("Refusé");
+    }
+    else {
+        console.log("Admis");
+    }
+    console.log("==============");
+}
